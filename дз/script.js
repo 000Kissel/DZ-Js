5877c46ad10199b31c25d67984e525fa64d330f9
000Kissel <147407107+000Kissel@users.noreply.github.com>
2024-10-12 11:27:58 +0500

    Add files via upload

---
+++ дз/script.js
@@ -0,0 +1,59 @@
+// 1
+
+let x = 5;
+let y = 'hello';
+let z = true;
+let w = null;
+let v;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(w);
+console.log(v);
+
+// 2
+
+let rst1 = '10';
+let rst2 = '20.5';
+let rst3 = '30px';
+
+console.log(Number(rst1));
+console.log(parseInt(rst2));
+console.log(parseFloat(rst3));
+
+// 3
+
+let str1 = '10.5';
+let str2 = '20';
+let str3 = '30.25px';
+
+str1 = parseInt(str1);
+console.log(str1);
+str2 = parseInt(str2);
+console.log(str2);
+str3 = parseFloat(str3);
+console.log(str3);
+
+// 5
+
+alert("я изучаю JS");
+
+// 6
+
+let nam = 123;
+alert('123');
+
+// 7
+
+let a = 1+2+3;
+alert(1+2+3);
+
+// 8
+
+let num1;
+num1 = +prompt('введите 1 число');
+num2 = +prompt('введите 2 число');
+console.log(num1 - num2);
+
+
